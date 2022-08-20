fb5e5c25e550f019bab7dc7f6347fdf7f2cac024
Jash Agrawal <agrawaljash99@gmail.com>
2022-08-20 07:26:50 +0530

    added mongoose connection and implemented all functions with db

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./database/config");
 const app = express();
 const port = process.env.PORT || 8080;
-
+connectDB();
 app.use(
   "/graphql",
   graphqlHTTP({
--- schema/schema.js
+++ schema/schema.js
@@ -1,30 +1,21 @@
-const { clients, projects } = require("../database/db");
+const Client = require("../models/client");
+const Project = require("../models/project");
+const ProjectType = require("./projectSchema");
+const ClientType = require("./clientSchema");
 const {
   GraphQLObjectType,
   GraphQLID,
-  GraphQLString,
   GraphQLSchema,
-  GraphQLScalarType,
   GraphQLList,
+  GraphQLString,
 } = require("graphql");
-
-const ClientType = new GraphQLObjectType({
-  name: "ClientType",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    phone: { type: GraphQLString },
-  }),
-});
-
 const RootQuery = new GraphQLObjectType({
   name: "Rqt",
   fields: () => ({
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args, context) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
@@ -33,7 +24,91 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args, context) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args, context) {
+        return Project.find();
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args, context) {
+        return Project.findById(args.id);
+      },
+    },
+  }),
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: () => ({
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+      },
+      resolve(parent, { name, email, phone }, context) {
+        const client = new Client({
+          name,
+          email,
+          phone,
+        });
+        return client.save();
+      },
+    },
+    addProject: {
+      type: ProjectType,
+      args: {
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        clientId: { type: GraphQLString },
+      },
+      resolve(parent, { name, description, status, clientId }, context) {
+        const project = new Project({
+          name,
+          description,
+          status,
+          clientId,
+        });
+        return project.save();
+      },
+    },
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, { id }, context) {
+        const project = Project.findByIdAndDelete(id);
+        return project;
+      },
+    },
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        clientId: { type: GraphQLString },
+      },
+      resolve(parent, { id, name, description, status, clientId }, context) {
+        const project = Project.findByIdAndUpdate(id, {
+          name,
+          description,
+          status,
+          clientId,
+        });
+        return project;
       },
     },
   }),
@@ -41,4 +116,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
