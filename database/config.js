41ecafc60b9532416b1f1a3f2aaed1dbe02ec170
Jash Agrawal <agrawaljash99@gmail.com>
2022-08-20 07:39:36 +0530

    removed previous files

--- database/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Connected to MongoDB at ${conn.connection.host}`);
-};
-module.exports = connectDB;
